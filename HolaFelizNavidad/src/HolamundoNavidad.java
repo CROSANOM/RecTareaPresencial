98aabfd7a57ba199404d0e5b457504f2e6e65b3e
crosanom <crosanom@hotmail.com>
2016-12-20 14:28:24 +0100

    Preparando el segundo commit de la branch pruebas

--- HolaFelizNavidad/src/HolamundoNavidad.java
+++ HolaFelizNavidad/src/HolamundoNavidad.java
@@ -1,10 +1,17 @@
-
+import java.util.Date;
 public class HolamundoNavidad {
 
 	public static void main(String[] args) {
 		String saludo ="jojojo";
 		System.out.println("Hola Mundo"+saludo);
 		// TODO Auto-generated method stub
+	
+int hora, minutos;
+	
+hora=14;
+minutos=17;
+String HoraExacta= (hora+":"+minutos);
+System.out.println(saludo+" "+"son las"+" "+HoraExacta);
 
 	}
 
